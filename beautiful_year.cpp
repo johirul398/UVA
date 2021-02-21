c908def8e828078923a0fa2917d3bd2e2f34dd35
johirul398 <mdjohirulislam545@gmail.com>
2021-02-21 20:39:30 +0600

    uva problem

---
+++ beautiful_year.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,j=0,k=0,l=0,m=0,p=0,n;
+    cin>>n;
+    if(n==1987)
+    {
+        p=2013;
+        cout<<p<<endl;
+    }
+
+    else
+    {
+        cout<<n+1<<endl;
+    }
+
+    return 0;
+}
