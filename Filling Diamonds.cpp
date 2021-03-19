92f36f500a40cb085db4cd7213ed22b0731fd357
johirul398 <mdjohirulislam545@gmail.com>
2021-03-20 02:32:40 +0600

    uva problem

---
+++ Filling Diamonds.cpp
@@ -0,0 +1,16 @@
+
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+        long long int a,b,c,d,e,f;
+        cin>>a;
+        while(a--)
+        {
+            cin>>b;
+            c=4*b-2;
+            cout<<c/4+1<<endl;
+            c=0;
+        }
+        return 0;
+    }
