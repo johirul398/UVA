aea6922fcf8d8e2f12db43de80b2c3402b875ae0
johirul398 <mdjohirulislam545@gmail.com>
2021-03-29 13:58:19 +0600

    uva problem

---
+++ s_len.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i=0,l;
+    char s[10];
+    gets(s);
+    while(s[i]!='\0')
+    {
+        i++;
+        l=i;
+    }
+   // l=i;
+    printf("%d",l);
+    return 0;
+}
