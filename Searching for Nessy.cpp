d4b289ea12d45c2cf8b726dfc79ddb6bd18946c3
johirul398 <mdjohirulislam545@gmail.com>
2021-04-23 15:04:33 +0600

    uva problem

---
+++ Searching for Nessy.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f,g,h,i,j,k;
+    cin>>k;
+    while(k--)
+    {
+        e=0,f=0;
+      cin>>a>>b;
+      e=a/3;
+      f=b/3;
+      cout<<e*f<<endl;
+    }
+    return 0;
+}
