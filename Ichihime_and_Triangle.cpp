21ea573de4712aee3266b2f354883c780f72bf2b
johirul398 <mdjohirulislam545@gmail.com>
2021-05-13 01:05:16 +0600

    source code

---
+++ Ichihime_and_Triangle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int i,j,k,a,b,c,d,e,f;
+    cin>>i;
+    while(i--)
+    {
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<d<<endl;
+    }
+    return 0;
+}
